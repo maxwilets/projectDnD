9e0a3ecfdb39081adb230e6baf9647867b199818
Max Wilets <mwilets@gmail.com>
2018-11-15 18:28:47 -0700

    started node test

---
+++ assets/test.js
@@ -0,0 +1,91 @@
+inquirer = require('inquirer');
+mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "games"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    firstAsk()
+})
+
+
+    function firstAsk(){
+    inquirer.prompt({
+
+            name: 'new',
+            type: 'confirm',
+            message: 'Is this a new game or an existing game?'
+        }
+    ).then(function(data){
+        if (data.new) {
+            inquirer.prompt([{
+                name: 'name',
+                type: 'input',
+                message: 'What do you want to call this game?'
+            }])
+            .then(function(data){
+                
+              connection.query('SELECT game_name FROM game', function(err,res){
+                    for(i = 0; i < res.length; i++){
+                        if (data.name == res[i].game_name){
+                            console.log('this game already exists!')
+                            return firstAsk()
+                        }
+                        else{
+                            connection.query('INSERT INTO game SET ?',{
+                                game_name : data.name,
+                                round: 0
+                            }, function(err, res){
+                                console.log('your game has been added')
+                            })
+                        }
+                    }
+              })
+                
+                
+            })
+        }
+        else{
+           gameCheck()
+        }
+    }
+)
+      
+    }
+    
+    
+
+
+function gameCheck(){
+    inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'What is he name of your game?'
+    })
+    .then(function(data){
+        connection.query("SELECT game_name FROM game", function(err,res){
+            for(i =0; i < res.length; i ++){
+                if(data.name == res[i].game_name){
+                    return whatToDo()
+                }
+                else{
+                    console.log('I cant seem to find that game')
+                    gameCheck()
+                }
+            }
+        })
+    })
+}
